aea0f9cced63e4a7438da93842e8597024ba3bd8
b-j-vishnu <vishnuhnu250@gmail.com>
2024-09-06 17:36:37 +0530

    modified route process

--- controller/ClientContoller.js
+++ controller/ClientContoller.js
@@ -5,22 +5,24 @@ const dotenv = require('dotenv')
 
 dotenv.config();
 const signupController = async (req, res) => {
-    const { email, password, fullName, mobile } = req.body;
-    try {
-        const dublicateClient = await client.findOne({ email: email })
-        if (dublicateClient) return res.status(409).json({ "message": "client Already Exit" })
-        const hashPassword = await bcrypt.hash(password, 10)
-        const registering = await new client({
-            fullName: fullName,
-            email: email,
-            password: hashPassword,
-            mobile: mobile
-        })
-        registering.save();
-        return res.status(200).json({ "message": "Signup Successfully" })
-    } catch (error) {
-        throw error;
-    }
+
+    console.log(req.body);
+
+    const dublicateClient = await client.findOne({ email: req.body.email })
+
+    if (dublicateClient) return res.status(409).json({ "message": "client Already Exit" })
+
+    const hashPassword = await bcrypt.hash(req.body.password, 10)
+
+    const registering = await new client({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: hashPassword,
+        mobile: req.body.mobile
+    })
+    registering.save()
+
+    return res.status(200).json({ "message": "Signup Successfully" })
 
 }
 
@@ -33,25 +35,25 @@ const loginController = async (req, res) => {
     const CheckPassword = await bcrypt.compare(req.body.password, existsClient.password)
     if (!CheckPassword) return res.status(401).json({ "message": "Password Incorrect" })
 
-    const token = jwt.sign({ email: existsClient.email }, process.env.token, { expiresIn: '1d' })
-    res.cookie('token', token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
-    return res.status(200).json({ "message": "login successfull" })
-
+    const token = jwt.sign({ id: existsClient._id }, process.env.token, { expiresIn: '1d' })
+    res.cookie('token', token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true })
+        .status(200).json({
+            message: "login successfully"
+        });
 }
 const logoutController = async (req, res) => {
-    res.clearCookie('token', { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
-    return res.json({ "message": 'logout successfully' })
+    return res.clearCookie('token', { maxAge: 24 * 60 * 60 * 1000, httpOnly: true }).json({ "message": 'logout successfully' })
 }
 
 const verifyToken = async (req, res) => {
     const token = req?.cookies?.token
+    if (!token) return res.status(401).json({ message: "No Token" });
     console.log(token);
-
-    if (!token) return res.status(401).json({ "message": "No Token" })
-    jwt.verify(token, process.env.token, (err, decoded) => {
-        if (err) return res.status(401).json({ "message": "Token has expired" })
-        return res.status(200).json({ "token": token, "message": "no token expired" })
-    })
-
+    const decoded = jwt.verify(token, process.env.token);
+    if (!decoded) return res.status(401).json({ message: "Token has expired" });
+    return res.status(200).json({
+        message: "access granted"
+    });
 }
+
 module.exports = { signupController, loginController, logoutController, verifyToken }
